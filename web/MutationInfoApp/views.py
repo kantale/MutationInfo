812fa85b46f84b4fb49f2f3f1a0b0398e1486f27
kantale <alexandros.kanterakis@gmail.com>
2016-02-11 00:06:15 +0200

    add VEP to interface

--- web/MutationInfoApp/views.py
+++ web/MutationInfoApp/views.py
@@ -6,6 +6,8 @@ import uuid
 import logging
 import StringIO
 
+from pyVEP import VEP
+
 from MutationInfo import MutationInfo 
 
 print 'Initializing MutationInfo..'
@@ -19,22 +21,24 @@ root_logger.setLevel(logging.DEBUG)
 # Create your views here.
 
 def do_MutationInfo(request):
-
+    '''
     test_ret = {'chrom': '12', 'source': 'NC_transcript', 'genome': 'GRCh37.p13', 'offset': 21355487, 'alt': 'G', 'ref': 'T'}
+    '''
 
     print request.method
 
     if request.method == u'GET':
 
         return render(request, 'MutationInfoApp/MutationInfoApp.html', {
-	#        'upload_form':upload_form, 
+
     	})
+
     elif request.method == u'POST':
         POST = request.POST
 
         variant_text = POST.getlist(u'variantText')[0]
 
-        #variant_text = u'NM_006446.4:c.1198T>G'
+#        variant_text = u'NM_006446.4:c.1198T>G'
 
         #Set a new logger
         string_buffer = StringIO.StringIO()
@@ -46,6 +50,7 @@ def do_MutationInfo(request):
         logging_handler.setFormatter(formatter)
         root_logger.addHandler(logging_handler)
 
+        #Run Mutation Info
         exception_message = 'MutationInfo run out of options!'
         mi_ret = None
         try:
@@ -53,14 +58,35 @@ def do_MutationInfo(request):
         except Exception as e:
             exception_message = 'MutationInfo failed: ' + str(e)
 
+        # Run VEP
+        if type(mi_ret) is dict:
+            # MutationInfo seems to run fine.
+            # Try VEP
+            VEP_variant = '%s:g.%s%s>%s' % tuple(map(str, (mi_ret['chrom'], mi_ret['offset'], mi_ret['ref'], mi_ret['alt'], )))
+            print 'VEP_variant:', VEP_variant
+            VEP_ret = VEP(VEP_variant)
+            VEP_msc = 'Could not run Variant Effect Predictor'
+            if type(VEP_ret) is list:
+                if len(VEP_ret) > 0:
+                    if type(VEP_ret[0]) is dict:
+                        if u'most_severe_consequence' in VEP_ret[0]:
+                            VEP_msc = VEP_ret[0][u'most_severe_consequence']
+            elif VEP_ret is dict:
+                if u'error' in VEP_ret:
+                    VEP_msc = VEP_ret[u'error']
+            #END OF VEP
+
+
         #Get log messages
         log_messages = string_buffer.getvalue()
         log_messages = '\n'.join([x.replace(this_uuid, '') for x in log_messages.split('\n') if this_uuid in x])
         root_logger.removeHandler(logging_handler)
 
         if type(mi_ret) is dict:
+            mi_ret['VEP_msc'] = VEP_msc
             mi_ret['log'] = log_messages
             mi_ret['success'] = True
+
         elif mi_ret is None:
             mi_ret = {
                 'log': log_messages,
