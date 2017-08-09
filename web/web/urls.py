c03f6780f2dd30df4e320d520988c731ab119ee2
kantale <alexandros.kanterakis@gmail.com>
2017-08-09 15:53:10 +0300

    remove /MutationInfo from django web

--- web/web/urls.py
+++ web/web/urls.py
@@ -2,11 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'web.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
 
-    url(r'^MutationInfo/', include('MutationInfoApp.urls', namespace='MutationInfoApp')),
+    url(r'', include('MutationInfoApp.urls', namespace='MutationInfoApp')),
 )
